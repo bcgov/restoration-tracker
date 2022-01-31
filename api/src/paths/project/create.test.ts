486d9c5721c7546d92f8cf3898eeeab1fe4247bf
Nick Phura <nickphura@gmail.com>
2022-01-31 15:15:51 -0800

    Update create endpoint tests. Update winston logger util function to change log level. Make mocha disable winston logger before test execution.

--- api/src/paths/project/create.test.ts
+++ api/src/paths/project/create.test.ts
@@ -1,94 +1,70 @@
+import Ajv from 'ajv';
 import chai, { expect } from 'chai';
 import { describe } from 'mocha';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import * as db from '../../database/db';
 import { HTTPError } from '../../errors/custom-error';
-import project_queries from '../../queries/project';
-import { getMockDBConnection } from '../../__mocks__/db';
-import * as create from './create';
+import { ProjectService } from '../../services/project-service';
+import { getMockDBConnection, getRequestHandlerMocks } from '../../__mocks__/db';
+import { createProject, POST } from './create';
 
 chai.use(sinonChai);
 
-describe('createProject', () => {
+describe.only('openapi schema', () => {
+  const ajv = new Ajv();
+
+  it('is valid openapi v3 schema', () => {
+    expect(ajv.validateSchema((POST.apiDoc as unknown) as object)).to.be.true;
+  });
+});
+
+describe.only('createProject', () => {
   afterEach(() => {
     sinon.restore();
   });
 
-  const dbConnectionObj = getMockDBConnection();
+  it('creates a new project', async () => {
+    const dbConnectionObj = getMockDBConnection();
 
-  const sampleReq = {
-    keycloak_token: {},
-    body1: {
-      coordinator: {
-        first_name: 'John',
-        last_name: 'Smith',
-        email_address: 'a@b.com',
-        coordinator_agency: 'A Rocha Canada',
-        share_contact_details: 'false'
-      },
-      permit: { permits: [] },
-      project: {
-        project_name: 'Tatyana Douglas',
-        start_date: '1900-01-01',
-        end_date: ''
-      },
-      location: {
-        regions: ['West Coast'],
-        geometry: [
-          { type: 'Feature', properties: {}, geometry: { type: 'Point', coordinates: [-124.716797, 52.88902] } }
-        ]
-      },
-      iucn: { classificationDetails: [] },
-      funding: { funding_sources: [] },
-      partnerships: { indigenous_partnerships: [], stakeholder_partnerships: [] }
-    },
-    params: {
-      projectId: 1
-    }
-  } as any;
+    sinon.stub(db, 'getDBConnection').returns(dbConnectionObj);
 
-  afterEach(() => {
-    sinon.restore();
-  });
+    sinon.stub(ProjectService.prototype, 'createProject').resolves(1);
 
-  it('should throw a 400 error when no request body present', async () => {
-    sinon.stub(db, 'getDBConnection').returns({
-      ...dbConnectionObj,
-      systemUserId: () => {
-        return 20;
-      }
-    });
+    const { mockReq, mockRes, mockNext } = getRequestHandlerMocks();
 
     try {
-      const result = create.createProject();
+      const requestHandler = createProject();
 
-      await result({ ...sampleReq, body: null }, (null as unknown) as any, (null as unknown) as any);
-      expect.fail();
+      await requestHandler(mockReq, mockRes, mockNext);
     } catch (actualError) {
-      expect((actualError as HTTPError).status).to.equal(400);
-      expect((actualError as HTTPError).message).to.equal('Failed to insert project general information data');
+      expect.fail();
     }
+
+    expect(mockRes.statusValue).to.equal(200);
+    expect(mockRes.jsonValue).to.eql({ id: 1 });
   });
 
-  it('should throw a 400 error when no sql statement returned for postProjectSQLStatement', async () => {
-    sinon.stub(db, 'getDBConnection').returns({
-      ...dbConnectionObj,
-      systemUserId: () => {
-        return 20;
-      }
-    });
+  it('catches error, calls rollback, and re-throws error', async () => {
+    const rollbackStub = sinon.stub();
 
-    sinon.stub(project_queries, 'postProjectSQL').returns(null);
+    const dbConnectionObj = getMockDBConnection({ rollback: rollbackStub });
+
+    sinon.stub(db, 'getDBConnection').returns(dbConnectionObj);
+
+    sinon.stub(ProjectService.prototype, 'createProject').rejects(new Error('a test error'));
+
+    const { mockReq, mockRes, mockNext } = getRequestHandlerMocks();
 
     try {
-      const result = create.createProject();
+      const requestHandler = createProject();
 
-      await result(sampleReq, (null as unknown) as any, (null as unknown) as any);
+      await requestHandler(mockReq, mockRes, mockNext);
       expect.fail();
     } catch (actualError) {
-      expect((actualError as HTTPError).status).to.equal(400);
-      expect((actualError as HTTPError).message).to.equal('Failed to build SQL insert statement');
+      expect(dbConnectionObj.rollback).to.have.been.called;
+
+      expect((actualError as HTTPError).message).to.equal('a test error');
     }
   });
 });
