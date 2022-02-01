7336b3c118f9d1ac3fadb6be43d405881467d288
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2022-02-01 12:08:33 -0800

    alignment

--- api/src/queries/search/search-queries.ts
+++ api/src/queries/search/search-queries.ts
@@ -18,23 +18,23 @@ export const getSpatialSearchResultsSQL = (isUserAdmin: boolean, systemUserId: n
   }
 
   const sqlStatement = SQL`
-  SELECT
-    p.project_id as id,
-    p.name,
-    public.ST_asGeoJSON(psc.geography) as geometry
-  FROM
-    project p
-  LEFT JOIN
-    project_spatial_component psc
-  ON
-    p.project_id = psc.project_id
-  LEFT join
-    project_spatial_component_type psct
-  ON
-    psc.project_spatial_component_type_id = psct.project_spatial_component_type_id
-  WHERE
-    psct.name = 'Boundary'
-  `;
+    SELECT
+      p.project_id as id,
+      p.name,
+      public.ST_asGeoJSON(psc.geography) as geometry
+    FROM
+      project p
+    LEFT JOIN
+      project_spatial_component psc
+    ON
+      p.project_id = psc.project_id
+    LEFT join
+      project_spatial_component_type psct
+    ON
+      psc.project_spatial_component_type_id = psct.project_spatial_component_type_id
+    WHERE
+      psct.name = 'Boundary'
+    `;
 
   if (!isUserAdmin) {
     sqlStatement.append(SQL` and p.create_user = ${systemUserId};`);
