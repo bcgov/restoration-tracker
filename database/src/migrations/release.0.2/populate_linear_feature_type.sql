86a5758bfe07b4a4a7b52fdda5b9451a07c0530c
Charlie Garrett-Jones <charlie.garrettjones@bayseum.com>
2022-02-07 12:34:44 -0800

    BHBC-1571 BHBC-1583
    - support multiple contacts and contact types
    - suppor project species

--- database/src/migrations/release.0.2/populate_linear_feature_type.sql
+++
@@ -1,8 +0,0 @@
--- populate_linear_feature_type.sql
-insert into linear_feature_type (name, record_effective_date) values ('Seismic Lines', now());
-insert into linear_feature_type (name, record_effective_date) values ('Roads', now());
-insert into linear_feature_type (name, record_effective_date) values ('Pipelines', now());
-insert into linear_feature_type (name, record_effective_date) values ('Transmission Lines', now());
-insert into linear_feature_type (name, record_effective_date) values ('Railways', now());
-insert into linear_feature_type (name, record_effective_date) values ('Trails', now());
-insert into linear_feature_type (name, record_effective_date) values ('Other', now());
--- database/src/migrations/release.0.2/populate_project_spatial_component_type.sql
+++
@@ -1,2 +0,0 @@
--- populate_project_spatial_component_type.sql
-insert into project_spatial_component_type (name, record_effective_date, description) values ('Boundary', now(), 'The spatial boundary of the project.');
