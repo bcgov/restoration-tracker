cacb4f207d5b9eee1e336fb379c2911b8c1ff39f
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2022-02-16 15:03:48 -0800

    update snapshots

--- app/src/features/projects/components/ProjectGeneralInformationForm.test.tsx
+++ app/src/features/projects/components/ProjectGeneralInformationForm.test.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 import { Formik } from 'formik';
 import React from 'react';
+import { codes } from 'test-helpers/code-helpers';
 import ProjectGeneralInformationForm, {
   IProjectGeneralInformationForm,
   ProjectGeneralInformationFormInitialValues,
@@ -16,7 +17,11 @@ describe('ProjectGeneralInformationForm', () => {
         validateOnBlur={true}
         validateOnChange={false}
         onSubmit={async () => {}}>
-        {() => <ProjectGeneralInformationForm />}
+        {() => <ProjectGeneralInformationForm  species={
+              codes?.species?.map((item) => {
+                return { value: item.id, label: item.name };
+              }) || []
+            }/>}
       </Formik>
     );
 
@@ -30,6 +35,9 @@ describe('ProjectGeneralInformationForm', () => {
         start_date: '2021-03-14',
         end_date: '2021-04-14',
         objectives: 'my objectives'
+      },
+      species:{
+        focal_species:[1234, 321]
       }
     };
 
@@ -40,7 +48,11 @@ describe('ProjectGeneralInformationForm', () => {
         validateOnBlur={true}
         validateOnChange={false}
         onSubmit={async () => {}}>
-        {() => <ProjectGeneralInformationForm />}
+        {() => <ProjectGeneralInformationForm  species={
+              codes?.species?.map((item) => {
+                return { value: item.id, label: item.name };
+              }) || []
+            } />}
       </Formik>
     );
 
