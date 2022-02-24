a9adefbb0d7ec91d8681cbd918c247a213aa3cef
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2022-02-24 11:31:03 -0800

    fix tests

--- app/src/components/layout/Header.test.tsx
+++ app/src/components/layout/Header.test.tsx
@@ -15,7 +15,6 @@ describe('Header', () => {
 
     mockHasSystemRole
       .mockReturnValueOnce(true) // Return true when the `Projects` secure link is parsed
-      .mockReturnValueOnce(true) // Return true when the `Permits` secure link is parsed
       .mockReturnValueOnce(true) // Return true when the `Manage Users` secure link is parsed
       .mockReturnValueOnce(true); // Return true when the `Map` secure link is parsed
 
@@ -51,52 +50,6 @@ describe('Header', () => {
     expect(getByText('Projects')).toBeVisible();
     expect(getByText('Map')).toBeVisible();
     expect(getByText('Manage Users')).toBeVisible();
-  });
-
-  it('renders correctly with system admin role', () => {
-    const mockHasSystemRole = jest.fn();
-
-    mockHasSystemRole
-      .mockReturnValueOnce(true) // Return true when the `Projects` secure link is parsed
-      .mockReturnValueOnce(true) // Return true when the `Permits` secure link is parsed
-      .mockReturnValueOnce(true) // Return true when the `Manage Users` secure link is parsed
-      .mockReturnValueOnce(true) // Return true when the `Map` secure link is parsed
-      .mockReturnValueOnce(true); // Return true when the `Resources` secure link is parsed
-
-    const authState = {
-      keycloakWrapper: {
-        keycloak: {
-          authenticated: true
-        },
-        hasLoadedAllUserInfo: true,
-        systemRoles: [SYSTEM_ROLE.SYSTEM_ADMIN],
-        getUserIdentifier: () => 'testuser',
-        hasAccessRequest: false,
-        isSystemUser: true,
-        hasSystemRole: mockHasSystemRole,
-        getIdentitySource: () => SYSTEM_IDENTITY_SOURCE.BCEID,
-        username: 'testusername',
-        displayName: 'testdisplayname',
-        email: 'test@email.com',
-        firstName: 'testfirst',
-        lastName: 'testlast',
-        refresh: () => {}
-      }
-    };
-
-    const { getByText } = render(
-      <AuthStateContext.Provider value={authState}>
-        <Router history={history}>
-          <Header />
-        </Router>
-      </AuthStateContext.Provider>
-    );
-
-    expect(getByText('Projects')).toBeVisible();
-    expect(getByText('Permits')).toBeVisible();
-    expect(getByText('Map')).toBeVisible();
-    expect(getByText('Manage Users')).toBeVisible();
-    expect(getByText('Resources')).toBeVisible();
   });
 
   it('renders the username and logout button', () => {
