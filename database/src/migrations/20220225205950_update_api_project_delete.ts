762393ab79da5043d94eced44119ebce7c129a7d
Nick Phura <nickphura@gmail.com>
2022-02-28 13:50:55 -0800

    Update migration (#37)

--- database/src/migrations/20220225205950_update_api_project_delete.ts
+++ database/src/migrations/20220225205950_update_api_project_delete.ts
@@ -4,8 +4,10 @@ import path from 'path';
 
 const DB_RELEASE = 'release.0.3.1';
 
+const DB_SCHEMA = process.env.DB_SCHEMA;
+
 /**
- * Apply restoration release changes.
+ * Update `api_delete_project` procedure.
  *
  * @export
  * @param {Knex} knex
@@ -15,11 +17,13 @@ export async function up(knex: Knex): Promise<void> {
   const api_delete_project = fs.readFileSync(path.join(__dirname, DB_RELEASE, 'api_delete_project.sql'));
 
   await knex.raw(`
+    set schema '${DB_SCHEMA}';
+    set search_path = ${DB_SCHEMA},public;
+
     ${api_delete_project}
   `);
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.raw(`
-  `);
+  await knex.raw('');
 }
