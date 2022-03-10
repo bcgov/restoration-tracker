19bc5136f6c680c051ae0567aaac1a16d9667159
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2022-03-09 16:40:03 -0800

    pagination working

--- app/src/features/projects/view/components/TreatmentList.tsx
+++ app/src/features/projects/view/components/TreatmentList.tsx
@@ -1,19 +1,28 @@
 import { Card, Typography } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 import { IGetProjectTreatment } from 'interfaces/useProjectApi.interface';
-import React from 'react';
+import React, { useState } from 'react';
+import { handleChangePage, handleChangeRowsPerPage } from 'utils/tablePaginationUtils';
 
 export interface IProjectTreatmentProps {
   treatmentList: IGetProjectTreatment[];
   getTreatments: (forceFetch: boolean) => void;
 }
+
+const useStyles = makeStyles({
+  container: {
+    maxHeight: 440
+  }
+});
 
 /**
  * General information content for a project.
@@ -21,7 +30,11 @@ export interface IProjectTreatmentProps {
  * @return {*}
  */
 const TreatmentList: React.FC<IProjectTreatmentProps> = (props) => {
+  const classes = useStyles();
   const { treatmentList } = props;
+
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [page, setPage] = useState(0);
 
   interface IGetTreatment {}
 
@@ -35,8 +48,8 @@ const TreatmentList: React.FC<IProjectTreatmentProps> = (props) => {
         </Typography>
       </Box>
       <Box>
-        <TableContainer>
-          <Table>
+        <TableContainer className={classes.container}>
+          <Table stickyHeader>
             <TableHead>
               <TableRow>
                 <TableCell>
@@ -71,32 +84,48 @@ const TreatmentList: React.FC<IProjectTreatmentProps> = (props) => {
                 </TableRow>
               )}
 
-              {treatmentList?.map((row?) => (
-                <TableRow key={row.id}>
-                  <TableCell>{row.id}</TableCell>
-                  <TableCell>{row.type}</TableCell>
-                  <TableCell>{row.width}</TableCell>
-                  <TableCell>{row.length}</TableCell>
-                  <TableCell>{row.area}</TableCell>
-                  <TableCell>{row.treatments?.map((item: any) => item.treatment_name).join(', ')}</TableCell>
-                  <TableCell>
-                    <Box my={-1}>
-                      <Button
-                        size="small"
-                        color="primary"
-                        variant="outlined"
-                        aria-label="view treatment unit details"
-                        data-testid="view-treatment-unit-details"
-                        onClick={() => viewTreatmentUnitDetailsDialog(row)}>
-                        {'View Details'}
-                      </Button>
-                    </Box>
-                  </TableCell>
-                </TableRow>
-              ))}
+              {treatmentList.length > 0 &&
+                treatmentList.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, index) => {
+                  return (
+                    <TableRow key={row.id}>
+                      <TableCell>{row.id}</TableCell>
+                      <TableCell>{row.type}</TableCell>
+                      <TableCell>{row.width}</TableCell>
+                      <TableCell>{row.length}</TableCell>
+                      <TableCell>{row.area}</TableCell>
+                      <TableCell>{row.treatments?.map((item: any) => item.treatment_name).join(', ')}</TableCell>
+                      <TableCell>
+                        <Box my={-1}>
+                          <Button
+                            size="small"
+                            color="primary"
+                            variant="outlined"
+                            aria-label="view treatment unit details"
+                            data-testid="view-treatment-unit-details"
+                            onClick={() => viewTreatmentUnitDetailsDialog(row)}>
+                            {'View Details'}
+                          </Button>
+                        </Box>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
             </TableBody>
           </Table>
         </TableContainer>
+        {treatmentList.length > 0 && (
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 15, 20]}
+            component="div"
+            count={treatmentList.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onChangePage={(event: unknown, newPage: number) => handleChangePage(event, newPage, setPage)}
+            onChangeRowsPerPage={(event: React.ChangeEvent<HTMLInputElement>) =>
+              handleChangeRowsPerPage(event, setPage, setRowsPerPage)
+            }
+          />
+        )}
       </Box>
     </Card>
   );
