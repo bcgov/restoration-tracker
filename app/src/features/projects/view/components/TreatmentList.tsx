cae6b5280223f12ecc3c7e96ad711441bb4f4fd2
jeznorth <twenty15creative@gmail.com>
2022-03-15 08:46:22 -0700

    Treatment Table UI Updates, General UI Cleanup

--- app/src/features/projects/view/components/TreatmentList.tsx
+++ app/src/features/projects/view/components/TreatmentList.tsx
@@ -24,8 +24,8 @@ const useStyles = makeStyles({
       verticalAlign: 'middle'
     }
   },
-  container: {
-    height: '300px'
+  pagination: {
+    flex: '0 0 auto'
   }
 });
 
@@ -46,91 +46,80 @@ const TreatmentList: React.FC<IProjectTreatmentListProps> = (props) => {
   const viewTreatmentUnitDetailsDialog = (treatment: IGetTreatment) => {};
 
   return (
-    <>
-      <Box display="flex" alignItems="center" justifyContent="space-between" p={2}>
-        Found {treatmentList?.length} {treatmentList?.length !== 1 ? 'treatments' : 'treatment'}
-      </Box>
-      <Box>
-        <TableContainer className={classes.container}>
-          <Table stickyHeader className={classes.treatmentsTable} aria-label="treatments-list-table">
-            <TableHead>
-              <TableRow>
-                <TableCell>
-                  <strong>ID</strong>
-                </TableCell>
-                <TableCell>
-                  <strong>Type</strong>
-                </TableCell>
-                <TableCell>
-                  <strong>Width</strong>
-                </TableCell>
-                <TableCell>
-                  <strong>Length</strong>
-                </TableCell>
-                <TableCell>
-                  <strong>Area</strong>
-                </TableCell>
-                <TableCell>
-                  <strong>Treatments</strong>
-                </TableCell>
-                <TableCell></TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody data-testid="project-table">
-              {!treatmentList?.length && (
-                <TableRow>
-                  <TableCell colSpan={7}>
-                    <Box display="flex" justifyContent="center">
-                      No Treatments
-                    </Box>
-                  </TableCell>
-                </TableRow>
-              )}
+    <Box display="flex" flexDirection="column" height="100%">
 
-              {treatmentList.length > 0 &&
-                treatmentList.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, index) => {
-                  return (
-                    <TableRow key={row.id}>
-                      <TableCell>{row.id}</TableCell>
-                      <TableCell>{row.type}</TableCell>
-                      <TableCell>{row.width}</TableCell>
-                      <TableCell>{row.length}</TableCell>
-                      <TableCell>{row.area}</TableCell>
-                      <TableCell>{row.treatments?.map((item: any) => item.treatment_name).join(', ')}</TableCell>
-                      <TableCell>
-                        <Box my={-1}>
-                          <Button
-                            size="small"
-                            color="primary"
-                            variant="outlined"
-                            aria-label="view treatment unit details"
-                            data-testid="view-treatment-unit-details"
-                            onClick={() => viewTreatmentUnitDetailsDialog(row)}>
-                            {'View Details'}
-                          </Button>
-                        </Box>
-                      </TableCell>
-                    </TableRow>
-                  );
-                })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        {treatmentList.length > 0 && (
-          <TablePagination
-            rowsPerPageOptions={[5, 10, 15, 20]}
-            component="div"
-            count={treatmentList.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onChangePage={(event: unknown, newPage: number) => handleChangePage(event, newPage, setPage)}
-            onChangeRowsPerPage={(event: React.ChangeEvent<HTMLInputElement>) =>
-              handleChangeRowsPerPage(event, setPage, setRowsPerPage)
-            }
-          />
-        )}
+      <Box display="flex" alignItems="center" justifyContent="space-between" p={2}>
+        <strong>Found {treatmentList?.length} {treatmentList?.length !== 1 ? 'treatments' : 'treatment'}</strong>
       </Box>
-    </>
+
+      <Box component={TableContainer} flex="1 1 auto">
+        <Table size="small" stickyHeader className={classes.treatmentsTable} aria-label="treatments-list-table">
+          <TableHead>
+            <TableRow>
+              <TableCell width="50">ID</TableCell>
+              <TableCell>Type</TableCell>
+              <TableCell>Treatments</TableCell>
+              <TableCell align="right">Width</TableCell>
+              <TableCell align="right">Length</TableCell>
+              <TableCell align="right">Area</TableCell>
+              <TableCell align="left" width="130">Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody data-testid="project-table">
+            {!treatmentList?.length && (
+              <TableRow>
+                <TableCell colSpan={7}>
+                  <Box display="flex" justifyContent="center">
+                    No Treatments
+                  </Box>
+                </TableCell>
+              </TableRow>
+            )}
+
+            {treatmentList.length > 0 &&
+              treatmentList.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, index) => {
+                return (
+                  <TableRow key={row.id}>
+                    <TableCell>{row.id}</TableCell>
+                    <TableCell>{row.type}</TableCell>
+                    <TableCell>{row.treatments?.map((item: any) => item.treatment_name).join(', ')}</TableCell>
+                    <TableCell align="right">{row.width}</TableCell>
+                    <TableCell align="right">{row.length}</TableCell>
+                    <TableCell align="right">{row.area}</TableCell>
+                    <TableCell align="left">
+                      <Button
+                        size="small"
+                        color="primary"
+                        variant="outlined"
+                        aria-label="view treatment unit details"
+                        data-testid="view-treatment-unit-details"
+                        onClick={() => viewTreatmentUnitDetailsDialog(row)}>
+                        {'View Details'}
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </Box>
+
+      {treatmentList.length > 0 && (
+        <TablePagination
+          className={classes.pagination}
+          rowsPerPageOptions={[5, 10, 15, 20]}
+          component="div"
+          count={treatmentList.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={(event: unknown, newPage: number) => handleChangePage(event, newPage, setPage)}
+          onChangeRowsPerPage={(event: React.ChangeEvent<HTMLInputElement>) =>
+            handleChangeRowsPerPage(event, setPage, setRowsPerPage)
+          }
+        />
+      )}
+
+    </Box>
   );
 };
 
