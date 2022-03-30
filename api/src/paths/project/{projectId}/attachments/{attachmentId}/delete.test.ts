ac074aebfa3b8db308b3540a351e261a7a65c5ee
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2022-03-30 16:08:43 -0700

    tests for delete attachments

--- api/src/paths/project/{projectId}/attachments/{attachmentId}/delete.test.ts
+++ api/src/paths/project/{projectId}/attachments/{attachmentId}/delete.test.ts
@@ -2,16 +2,11 @@ import chai, { expect } from 'chai';
 import { describe } from 'mocha';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
-import * as delete_attachment from './delete';
 import * as db from '../../../../../database/db';
-// import project_queries from '../../../../../queries/project';
-// import security_queries from '../../../../../queries/security';
-// import SQL from 'sql-template-strings';
-// import * as file_utils from '../../../../../utils/file-utils';
-// import { DeleteObjectOutput } from 'aws-sdk/clients/s3';
-import { getMockDBConnection } from '../../../../../__mocks__/db';
 import { HTTPError } from '../../../../../errors/custom-error';
 import { AttachmentService } from '../../../../../services/attachment-service';
+import { getMockDBConnection, getRequestHandlerMocks } from '../../../../../__mocks__/db';
+import * as delete_attachment from './delete';
 
 chai.use(sinonChai);
 
@@ -99,5 +94,30 @@ describe('deleteAttachment', () => {
     await result(sampleReq, sampleRes as any, (null as unknown) as any);
 
     expect(actualResult).to.equal(null);
+  });
+
+  it('should throw an error when deleteAttachment fails', async () => {
+    const dbConnectionObj = getMockDBConnection({ rollback: sinon.stub(), release: sinon.stub() });
+
+    sinon.stub(db, 'getDBConnection').returns(dbConnectionObj);
+
+    sinon.stub(AttachmentService.prototype, 'deleteAttachment').rejects(new Error('a test error'));
+
+    const { mockReq, mockRes, mockNext } = getRequestHandlerMocks();
+
+    mockReq.params = {
+      projectId: '1',
+      attachmentId: '1'
+    };
+
+    try {
+      const requestHandler = delete_attachment.deleteAttachment();
+      await requestHandler(mockReq, mockRes, mockNext);
+      expect.fail();
+    } catch (actualError) {
+      expect(dbConnectionObj.rollback).to.have.been.called;
+      expect(dbConnectionObj.release).to.have.been.called;
+      expect((actualError as HTTPError).message).to.equal('a test error');
+    }
   });
 });
