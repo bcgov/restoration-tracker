45d1429aa89a65f119968fdc72a3d8ec7637ed22
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2022-03-30 16:21:42 -0700

    test for the public list attachments

--- api/src/paths/public/project/{projectId}/attachments/list.test.ts
+++ api/src/paths/public/project/{projectId}/attachments/list.test.ts
@@ -3,11 +3,11 @@ import { describe } from 'mocha';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import * as db from '../../../../../database/db';
-import { getMockDBConnection } from '../../../../../__mocks__/db';
 import { HTTPError } from '../../../../../errors/custom-error';
-import { getPublicProjectAttachments } from './list';
-import { AttachmentService } from '../../../../../services/attachment-service';
 import { GetAttachmentsData } from '../../../../../models/project-attachments';
+import { AttachmentService } from '../../../../../services/attachment-service';
+import { getMockDBConnection, getRequestHandlerMocks } from '../../../../../__mocks__/db';
+import { getPublicProjectAttachments } from './list';
 
 chai.use(sinonChai);
 
@@ -62,5 +62,28 @@ describe('getPublicProjectAttachments', () => {
     await getPublicProjectAttachments()(sampleReq, sampleRes as any, (null as unknown) as any);
 
     expect(actualResult).to.be.eql(new GetAttachmentsData());
+  });
+  it('should throw an error when list attachments fails', async () => {
+    const dbConnectionObj = getMockDBConnection({ rollback: sinon.stub(), release: sinon.stub() });
+
+    sinon.stub(db, 'getDBConnection').returns(dbConnectionObj);
+
+    sinon.stub(AttachmentService.prototype, 'getAttachments').rejects(new Error('a test error'));
+
+    const { mockReq, mockRes, mockNext } = getRequestHandlerMocks();
+
+    mockReq.params = {
+      projectId: '1'
+    };
+
+    try {
+      const requestHandler = getPublicProjectAttachments();
+      await requestHandler(mockReq, mockRes, mockNext);
+      expect.fail();
+    } catch (actualError) {
+      expect(dbConnectionObj.rollback).to.have.been.called;
+      expect(dbConnectionObj.release).to.have.been.called;
+      expect((actualError as HTTPError).message).to.equal('a test error');
+    }
   });
 });
