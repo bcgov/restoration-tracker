202f2970ed3a8d389d14feb9680ef21d97b1a889
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2022-03-31 13:27:34 -0700

    a couple of tests for eml.ts

---
+++ api/src/paths/project/{projectId}/export/eml.test.ts
@@ -0,0 +1,68 @@
+import chai, { expect } from 'chai';
+import { describe } from 'mocha';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import * as db from '../../../../database/db';
+
+import { HTTPError } from '../../../../errors/custom-error';
+import { getProjectEml } from './eml';
+import { EmlService } from '../../../../services/eml-service';
+import { getMockDBConnection, getRequestHandlerMocks } from '../../../../__mocks__/db';
+
+chai.use(sinonChai);
+
+describe('getProjectEml', () => {
+  const dbConnectionObj = getMockDBConnection();
+
+  const sampleReq = {
+    keycloak_token: {},
+    body: {},
+    params: {
+      projectId: 1
+    }
+  } as any;
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('should throw a 400 error when no projectId is provided', async () => {
+    sinon.stub(db, 'getDBConnection').returns(dbConnectionObj);
+
+    try {
+      await getProjectEml()(
+        { ...sampleReq, params: { ...sampleReq.params, projectId: null } },
+        (null as unknown) as any,
+        (null as unknown) as any
+      );
+      expect.fail();
+    } catch (actualError) {
+      expect((actualError as HTTPError).status).to.equal(400);
+      expect((actualError as HTTPError).message).to.equal('Failed to build SQL get statement');
+    }
+  });
+
+  it('should throw an error when buildProjectEml fails', async () => {
+    const dbConnectionObj = getMockDBConnection({ rollback: sinon.stub(), release: sinon.stub() });
+
+    sinon.stub(db, 'getDBConnection').returns(dbConnectionObj);
+
+    sinon.stub(EmlService.prototype, 'buildProjectEml').rejects(new Error('a test error'));
+
+    const { mockReq, mockRes, mockNext } = getRequestHandlerMocks();
+
+    mockReq.params = {
+      projectId: '1'
+    };
+
+    try {
+      const requestHandler = getProjectEml();
+      await requestHandler(mockReq, mockRes, mockNext);
+      expect.fail();
+    } catch (actualError) {
+      expect(dbConnectionObj.rollback).to.have.been.called;
+      expect(dbConnectionObj.release).to.have.been.called;
+      expect((actualError as HTTPError).message).to.equal('a test error');
+    }
+  });
+});
